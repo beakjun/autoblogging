b354e3a1badfc927e1e790e2deed5c1dae7f071c
bj <wjsqorwns93@naver.com>
2024-09-24 16:58:08 +0900

    test : fastapi test 완료

---
+++ test/fastapi_test.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/munang")
+async def root():
+    return {"message": "무냉"}

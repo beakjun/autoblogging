c3511e930f6b19a176e7990bec3e1479a7eded5c
bj <wjsqorwns93@naver.com>
2024-10-21 17:23:54 +0900

    Update : extract_store_id added

--- autoblog.py
+++ autoblog.py
@@ -1,4 +1,5 @@
-import textwrap
+import requests
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import google.generativeai as genai
@@ -11,7 +12,7 @@ from pyvirtualdisplay import Display
 
 GOOGLE_API_KEY = 'AIzaSyCW9fwk8jBkVQ45fiKvVHFLj1971yI1X-o'
 
-class Restaurant:
+class Restaurant_Info:
     """
     음식점 클래스 초기화 메서드
 
@@ -26,7 +27,7 @@ class Restaurant:
         self.date = date
         self.menu = []
         self.restaurant_reviews = []
-        self.menu_reviews = []
+        self.menu_reviews = {}
 
     def add_menu(self, menu):
         self.menu.append(menu)
@@ -38,20 +39,29 @@ class Restaurant:
         menu_str = ', '.join(self.menu)
         return f"음식점 이름: {self.name} , 장소: {self.location}, 대표 메뉴: {menu_str}"
     
-    def crawl_reviews(self):
+    def crawling_restaurant(self):
         # pyvirtualdisplay
         display = Display(visible=0, size=(1920, 1080))
         display.start()
-
+        agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
         # selenium options
         chrome_options = Options()
-        chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36')
-        chrome_options.add_argument("--disable-dev-shm-usage")
-
+        chrome_options.add_argument(f'--user-agent={agent}')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         # selenium driver
         driver = webdriver.Chrome(options=chrome_options)
-        url = f"https://m.place.naver.com/restaurant/1198311937/home"
-        time.sleep(5)
+        
+        # getting naver store_id
+
+        
+        
+        store_id=self.extract_store_id()
+        
+        
+        
+        url = f"https://m.place.naver.com/restaurant/{store_id}/home"
+        time.sleep(2)
+        
         driver.get(url)
         
         # 가게 위치 추출
@@ -68,6 +78,40 @@ class Restaurant:
         
         driver.quit()
         display.stop()
+        
+        
+    def extract_store_id(self):
+        url = f'https://m.map.naver.com/search2/search.naver?query={self.location} {self.name}&sm=hty&style=v5'
+        agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+        headers = { 'User-Agent': agent }
+
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        script_tag = soup.find('script', string=lambda t: t is not None and 'requirejs' in t)
+        if script_tag:
+            # script 내의 코드로부터 searchResult 데이터 찾아서 추출
+            code = script_tag.string.split('requirejs([')[-1]  # 'requirejs([' 이후 내용 가져오기
+            if 'search' in code:
+                # 예시로 가정된 searchResult 객체를 찾는 부분
+                search_result_start = code.find('searchResult = {')
+                search_result_end = code.find('};', search_result_start) + 1
+                search_result_json = code[search_result_start:search_result_end]
+                
+                        # JSON 문자열을 Python 객체로 변환
+                search_result = json.loads(search_result_json.replace('searchResult = ', ''))
+                
+                # 버우드의 고유 ID 추출
+                store_id = search_result['site']['list'][0]['id']
+                print(f"{self.name} 네이버 고유 ID:", store_id)
+        else:
+            store_id=None
+            print("requirejs가 포함된 <script> 태그를 찾을 수 없습니다.")
+        
+        return store_id
+    
+    
 
     def extract_location(self, driver):
         page_source = driver.page_source
@@ -99,6 +143,17 @@ class Restaurant:
         # 예시로 빈 문자열 반환
         return "리뷰 정보를 찾을 수 없습니다."
 
-restuarant = Restaurant("가장맛있는 족발", "등촌", ["스파게티"])
-print(restuarant.crawl_reviews())
- 
+
+
+
+
+
+
+
+
+
+
+
+restaurant=Restaurant_Info("텐노아지","등촌역","2024-08-02")
+restaurant.add_menu('파스타')
+restaurant.crawling_restaurant()
